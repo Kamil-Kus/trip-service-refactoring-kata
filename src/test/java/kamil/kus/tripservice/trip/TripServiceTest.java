8de63e12af71ecd75dd470ae1d6629b45af28238
Kamil Kus <KamilKusDev@protonmail.com>
2022-03-01 01:29:19 +0100

    Set java 17, update .gitignore, added TestableTripService class

--- src/test/java/kamil/kus/tripservice/trip/TripServiceTest.java
+++ src/test/java/kamil/kus/tripservice/trip/TripServiceTest.java
@@ -1,5 +1,24 @@
 package kamil.kus.tripservice.trip;
 
+import kamil.kus.tripservice.exception.UserNotLoggedInException;
+import kamil.kus.tripservice.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+    @Test
+    public void should_throw_exception_when_user_is_not_logged() {
+        TestableService tripService = new TestableService();
+       assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+    }
+}
+
+class TestableService extends TripService {
+
+    @Override
+    protected User getLoggedUser() {
+        return null;
+    }
+
 }
