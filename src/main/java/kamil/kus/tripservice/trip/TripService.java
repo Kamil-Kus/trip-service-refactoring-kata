8f40e70ba3c70efc8b42272f19f9983c3e46692e
Kamil Kus <KamilKusDev@protonmail.com>
2022-03-02 22:09:46 +0100

    Cover core method of TripService

--- src/main/java/kamil/kus/tripservice/trip/TripService.java
+++ src/main/java/kamil/kus/tripservice/trip/TripService.java
@@ -9,28 +9,32 @@ import kamil.kus.tripservice.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<Trip>();
+        User loggedUser = getLoggedUser();
+        boolean isFriend = false;
+        if (loggedUser != null) {
+            for (User friend : user.getFriends()) {
+                if (friend.equals(loggedUser)) {
+                    isFriend = true;
+                    break;
+                }
+            }
+            if (isFriend) {
+                tripList = extractTripsByUser(user);
+            }
+            return tripList;
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
 
-	protected User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
+    protected List<Trip> extractTripsByUser(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    protected User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
 
 }
--- src/test/java/kamil/kus/tripservice/trip/TripServiceTest.java
+++ src/test/java/kamil/kus/tripservice/trip/TripServiceTest.java
@@ -4,6 +4,7 @@ import kamil.kus.tripservice.exception.UserNotLoggedInException;
 import kamil.kus.tripservice.user.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -11,33 +12,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TripServiceTest {
 
-    private final User notLoggedUser = null;
-    private final User loggedUser = new User();
-    private final User userWithoutFriends = new User();
+    private static final User NOT_LOGGED_USER = null;
+    private static final User LOGGED_USER = new User();
+    private static final User USER_WITHOUT_FRIENDS = new User();
+    private static final User USER_WITH_FRIENDS = new User();
+    private static final List<Trip> TRIP_LIST = new ArrayList<>();
+    private static final Trip ITALY = new Trip();
 
     @Test
+
     public void should_throw_exception_when_user_is_not_logged() {
-        TestableService tripService = new TestableService(notLoggedUser);
-        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+        //When
+        TestableService tripService = new TestableService(NOT_LOGGED_USER, TRIP_LIST);
+        //Given and Then
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(USER_WITHOUT_FRIENDS));
     }
 
     @Test
     public void should_return_empty_list_when_user_is_not_friend() {
-        TestableService tripService = new TestableService(loggedUser);
-        final List<Trip> tripsByUser = tripService.getTripsByUser(userWithoutFriends);
+        //When
+        TestableService tripService = new TestableService(LOGGED_USER, TRIP_LIST);
+        //Given
+        final List<Trip> tripsByUser = tripService.getTripsByUser(USER_WITHOUT_FRIENDS);
+        //Then
         assertThat(tripsByUser.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void should_return_trip_list_when_user_is_friend() {
+        //When
+        TRIP_LIST.add(ITALY);
+        USER_WITH_FRIENDS.addFriend(LOGGED_USER);
+        TestableService tripService = new TestableService(LOGGED_USER, TRIP_LIST);
+        //Given
+        final List<Trip> tripsByUser = tripService.getTripsByUser(USER_WITH_FRIENDS);
+        //Then
+        assertThat(tripsByUser.isEmpty()).isFalse();
     }
 }
 
 class TestableService extends TripService {
     private final User loggedUser;
+    private final List<Trip> tripList;
 
     @Override
     protected User getLoggedUser() {
         return loggedUser;
     }
 
-    public TestableService(User loggedUser) {
+    @Override
+    protected List<Trip> extractTripsByUser(User user) {
+        return tripList;
+    }
+
+    public TestableService(User loggedUser, List<Trip> tripList) {
         this.loggedUser = loggedUser;
+        this.tripList = tripList;
     }
 }
